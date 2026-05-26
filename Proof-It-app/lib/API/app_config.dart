06285b419f6a37888c4d9ac37c7f3f903ae4770c
Krees01 <kristama02@gmail.com>
2026-05-26 09:29:17 +0700

    Change URL to railway.app

--- Proof-It-app/lib/API/app_config.dart
+++ Proof-It-app/lib/API/app_config.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/foundation.dart' show kIsWeb;
+
 class AppConfig {
   static String get host => kIsWeb ? 'localhost' : '10.0.2.2';
-  static String get baseUrl => 'http://$host:3000/api';
+  static String get baseUrl =>
+      'https://gregarious-playfulness-production-e7df.up.railway.app/api';
 }
